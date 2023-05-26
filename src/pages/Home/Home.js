7f4bbcf73555d45c8073da469db9b506805fbdb4
Thomas Bruand <thomas.bruand@gmail.com>
2023-05-26 18:59:34 +0200

    Ajout du contenu static de la page home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,31 @@
+import Card from '../../components/Card/Card';
+import "./Home.css"
 
 function Home() {
-
+    
     return (
         <>
-            <div>
-                <h1>Je suis la home page</h1>
-            </div>
+        <main>        
+        <div id="main-top">  
+        <div id="main-top-overlay">      
+        <h1 id="home-h1">Chez vous, partout et ailleurs</h1>
+        </div>
+        </div>
+        <div id="main-bottom">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+        </main>
         </>
-    )
-}
+        )
+    }
 
 export default Home;
