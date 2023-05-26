57b49173a7035eaccca380b567cc428e529450b5
Thomas Bruand <thomas.bruand@gmail.com>
2023-05-26 19:11:01 +0200

    Ajout du component Card + ajout du style du component Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Card.css"
+
+const Card = () => {
+    return (
+        <>        
+        <div id='card-home'>        
+        <h2 id='card-h2'>Titre de la 
+        location</h2>            
+        </div>
+        </>
+    );
+};
+
+export default Card;
