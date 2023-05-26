25499ca643f683764c46f986fa2abf3a8d38e686
Thomas Bruand <thomas.bruand@gmail.com>
2023-05-26 19:09:49 +0200

    Ajout du component Footer + ajout du style du component Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>            
+            <img id='logo-footer' src="./assets/logo/logo-footer.png" alt="Logo Kasa" />
+            <p id='p-footer'>© 2020 Kasa. All rights reserved</p>            
+        </footer>
+    );
+};
+
+export default Footer;
