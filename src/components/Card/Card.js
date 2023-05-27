7fad9bd4b16fd7717768ca5095fe7d9ce39e37a8
Thomas Bruand <thomas.bruand@gmail.com>
2023-05-27 08:05:40 +0200

    Ajout du props, ajout d'un balise img + div overlay, modification du style pour correspondre a la maquette desktop/mobile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Card.css"
 
-const Card = () => {
+const Card = (location) => {
+   
     return (
         <>        
-        <div id='card-home'>        
-        <h2 id='card-h2'>Titre de la 
-        location</h2>            
+        <div id='card-home'>  
+        <img id='card-img' src={location.img} alt={location.title} />  
+        <div id="overlay"></div>    
+        <h2 id='card-h2'>{location.title}</h2>            
         </div>
         </>
     );
