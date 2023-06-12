cfc74067d009a2ff7241210c47a9df9e35e47cf6
Thomas Bruand <thomas.bruand@gmail.com>
2023-06-12 17:11:06 +0200

    Ajout du compenent Tag

---
+++ src/components/Tag/Tag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tag = ({tag}) => {
+
+    
+    return (        
+        <>
+        <p className='tags'>{tag}</p>
+        </>
+    );
+};
+
+export default Tag;
