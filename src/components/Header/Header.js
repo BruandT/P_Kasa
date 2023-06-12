54137e2991b487288ffb7f725cff3d126cd695a6
Thomas Bruand <thomas.bruand@gmail.com>
2023-06-12 17:21:57 +0200

    Ajout d'une redirection sur la page Home lors du clique sur le logo, et modification du responsive en css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import Navigation from "../Navigation/Navigation";
+import { NavLink } from 'react-router-dom';
 import "./Header.css"
 
 function Header() {
     return (
         <>
             <header>
-                <img src="./assets/logo/logo.jpg" alt="Logo Kasa" />
+                <NavLink to ="/">
+                <img src="./assets/logo/logo.png" alt="Logo Kasa" />
+                </NavLink>
                 <Navigation />
             </header>
         </>
