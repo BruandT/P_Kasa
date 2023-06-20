967b1b751219aa31e2e57acf024800e8b10bb605
Thomas Bruand <thomas.bruand@gmail.com>
2023-06-20 08:08:25 +0200

    Modification du NavLink to

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -8,7 +8,7 @@ function Error404(){
         <main className='error-main'>
         <h1 className='error-h1'>404</h1>
         <p className='error-p'>Oups! La page que vous demandez n'existe pas.</p>
-        <NavLink to="/home">
+        <NavLink to="/">
             <button className='error-btn'>Retourner sur la page d'accueil</button>
         </NavLink>
         
